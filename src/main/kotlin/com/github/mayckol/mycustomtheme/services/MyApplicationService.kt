409e9d63b6b06ef69a6f479ea1a5ffebca1e72f9
GitHub Action <action@github.com>
2022-01-20 15:01:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/mayckol/mycustomtheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mayckol/mycustomtheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mayckol.mycustomtheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mayckol.mycustomtheme.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mayckol/mycustomtheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mayckol/mycustomtheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mayckol.mycustomtheme.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mayckol.mycustomtheme.MyBundle
 
 class MyProjectService(project: Project) {
 
